140697f2d11a49af7e30d64bf90e1a337d736152
sadmantm <108137761+sadmantm@users.noreply.github.com>
2023-05-16 11:32:23 -0300

    Add files via upload

---
+++ m3u.py
@@ -0,0 +1,18 @@
+import re
+print("CARREGANDO...")
+def procurarlink(listas):
+   padraolink = r"http://.*?(?:=ts|plus|m3u)\b"
+   with open(listas,"r") as arquivo:
+      conteudo = arquivo.read()
+      links_encontrados= re.findall(padraolink,conteudo)
+
+      return links_encontrados
+   nome_do_arquivo ="listas.txt"
+   links_encontrados = procurar_links_arquivo(nome_do_arquivo)
+   for link in links_encontrados:
+
+    print(link)
+   
+
+
+
