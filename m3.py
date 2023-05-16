068c89e07a0daa0bc92d4a13f062a395db4bf020
sadmantm <108137761+sadmantm@users.noreply.github.com>
2023-05-16 12:03:11 -0300

    Update m3.py

--- m3.py
+++ m3.py
@@ -17,7 +17,25 @@ def procurar_links_arquivo(nome_arquivo):
 # Exemplo de uso
 nome_do_arquivo = "listas.txt"  # substitua pelo nome do arquivo que deseja procurar os links
 links_encontrados = procurar_links_arquivo(nome_do_arquivo)
+# Função para extrair o domínio, nome de usuário e senha de uma URL
+def extrair_informacoes_link(url):
+    parsed_url = urlparse(url)
+    domain = parsed_url.netloc
+    query_params = parse_qs(parsed_url.query)
+    username = query_params.get('username', [''])[0]
+    password = query_params.get('password', [''])[0]
 
-# Imprimir os links encontrados
+    return domain, username, password
+
+# Exemplo de uso
+nome_do_arquivo = "listas.txt"  # substitua pelo nome do arquivo que deseja procurar os links
+links_encontrados = procurar_links_arquivo(nome_do_arquivo)
+
+# Processar cada link e extrair as informações desejadas
 for link in links_encontrados:
-    print((for int i == 0; if i < 1; int ++ +) link + "\n\n")
+    domain, username, password = extrair_informacoes_link(link)
+    print("Link:", link)
+    print("Domínio:", domain)
+    print("Nome de usuário:", username)
+    print("Senha:", password)
+    print("\n\n")  # Pular linha entre os resultados
