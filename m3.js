ced64002d4f5822b14898b90893c6f225af97481
sadmantm <108137761+sadmantm@users.noreply.github.com>
2023-05-17 03:24:27 +0100

    Update m3.js

--- m3.js
+++ m3.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const { URL } = require('url');
+const axios = require('axios');
+const iptv = require('iptv-playlist-parser');
 
 function procurarLinksArquivo(nomeArquivo) {
   const conteudo = fs.readFileSync(nomeArquivo, 'utf8');
@@ -17,19 +19,34 @@ function extrairInformacoesLink(link) {
   return { domain, username, password };
 }
 
+async function verificarLinkIPTV(link) {
+  try {
+    const response = await axios.get(link, { validateStatus: false });
+    const playlist = iptv.parse(response.data);
+    if (playlist.items && playlist.items.length > 0) {
+      return 'Funcionando';
+    } else {
+      return 'Playlist vazia ou inválida';
+    }
+  } catch (error) {
+    return 'Erro de conexão';
+  }
+}
+
 const nomeDoArquivo = 'listas.txt';
 const linksEncontrados = procurarLinksArquivo(nomeDoArquivo);
+(async () => {
+  for (const link of linksEncontrados) {
+    const { domain, username, password } = extrairInformacoesLink(link);
+    console.log('Host:', domain);
+    console.log('Nome de usuário:', username);
+    console.log('Senha:', password);
+    console.log('Link:', link);
+    const status = await verificarLinkIPTV(link);
+    console.log('Status:', status);
 
-linksEncontrados.forEach((link) => {
-  const { domain, username, password } = extrairInformacoesLink(link);
-  console.log('URL:',domain);
-  console.log('Nome de usuário:', username);
-  console.log('Senha:', password);
+    console.log('\n');
+  }
 
-if(domain === 'http://meuplay.shop' || domain === 'http://5ce.me') {
-console.log('porta: 80');
-}  
-console.log('\n');
-});
-
-console.log('Resultados:', linksEncontrados.length);
+  console.log('Resultados:', linksEncontrados.length);
+})();
